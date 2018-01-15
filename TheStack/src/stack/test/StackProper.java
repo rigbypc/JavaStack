50ba9ada5f965ebccdbcc7398e890dab909dacc9
Peter C Rigby <rigbypc@gmail.com>
2018-01-15 15:20:55 -0500

    Super iterator

--- TheStack/src/stack/test/StackProper.java
+++ TheStack/src/stack/test/StackProper.java
@@ -1,7 +1,14 @@
 package stack.test;
 
+import java.util.Iterator;
 import java.util.Stack;
 
 public class StackProper<E> extends Stack<E> {
+
+	@Override
+	public synchronized Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return super.iterator();
+	}
 
 }
