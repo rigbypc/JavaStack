d4774b07a3f55864abcf9cb88ecbedcb9df8dd5b
Peter C Rigby <rigbypc@gmail.com>
2018-01-15 15:12:10 -0500

    Move tests to nonexistant class StackProper

---
+++ TheStack/src/stack/test/TestStackProper.java
@@ -0,0 +1,63 @@
+package stack.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.Stack;
+import java.util.Vector;
+
+import org.junit.Test;
+
+public class TestStackProper {
+
+	@Test
+	public void test() {
+		StackProper<Integer> stack = new StackProper<Integer>();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		
+		System.out.println("Push and Pop");
+		System.out.println(stack.peek());
+		assertEquals(3, stack.pop().intValue());
+		System.out.println(stack.peek());
+		assertEquals(2, stack.pop().intValue());
+		System.out.println(stack.peek());
+		assertEquals(1, stack.pop().intValue());
+		
+		assertTrue(stack.isEmpty());
+		
+	}
+	
+	@Test
+	//a characterization test
+	public void testStackIterator() {
+		StackProper<Integer> stack = new StackProper<Integer>();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		
+		System.out.println("Iterator");
+		//Iterator is broken on stack it behaves like a queue
+		int val = 1;
+		for (Integer i : stack) {
+			System.out.println(i);
+			assertEquals(val, i.intValue());
+			val ++;	
+		}
+		
+		//Iterator just looks at elements, no pops
+		assertFalse(stack.isEmpty());
+	}
+	
+	@Test
+	public void testStackIteratorType() {
+		StackProper<Integer> stack = new StackProper<Integer>();
+		Iterator it = stack.iterator();
+		System.out.println("Iterator is of type " 
+				+ it.getClass().getName());
+		Vector vec = new Vector<>();
+		assertEquals(it.getClass(), vec.iterator().getClass());
+	}
+
+}
